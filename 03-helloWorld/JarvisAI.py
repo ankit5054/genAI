01ddb147c04850dec3317af1c7b4055f59446620
Ankit Mishra <ankit.mishra9780@gmail.com>
2025-06-25 14:30:27 +0530

    removing badges in prod3

--- 03-helloWorld/JarvisAI.py
+++ 03-helloWorld/JarvisAI.py
@@ -7,22 +7,49 @@ load_dotenv()
 st.markdown(
     """
     <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
-    }
-    #GithubIcon {
-        visibility: hidden;
-    }
-    #MainMenu {
-        visibility: hidden;
-    }
+        .css-1jc7ptx,
+        .e1ewe7hr3,
+        .viewerBadge_container__1QSob,
+        .styles_viewerBadge__1yB5_,
+        .viewerBadge_link__1S137,
+        .viewerBadge_text__1JaDK {
+            display: none;
+        }
 
+        #MainMenu {
+            visibility: hidden;
+        }
+
+        footer {
+            visibility: hidden;
+        }
+
+        header {
+            visibility: hidden;
+        }
     </style>
     """,
     unsafe_allow_html=True
 )
+
+# st.markdown(
+#     """
+#     <style>
+#         .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+#         .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+#         .viewerBadge_text__1JaDK {
+#             display: none;
+#         }
+#         #GithubIcon {
+#             visibility: hidden;
+#         }
+#         #MainMenu {
+#             visibility: hidden;
+#         }
+#     </style>
+#     """,
+#     unsafe_allow_html=True
+# )
 
 
 # Initialize OpenAI client
